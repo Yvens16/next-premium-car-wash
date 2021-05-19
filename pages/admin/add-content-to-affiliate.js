b36352eade83796d87a1abae5bdf89227d019557
Yvens16 <yvensbelaston@gmail.com>
2021-05-19 12:04:31 +0200

    initialized admin component for build

--- pages/admin/add-content-to-affiliate.js
+++ pages/admin/add-content-to-affiliate.js
@@ -1,3 +1,9 @@
 //TODO: https://www.youtube.com/watch?v=K7g8X_VRDy8 faire responsive
 //TODO: faire une page protéger pour ajouter des photos d'avant après et une mini vidéo pour receuillir l'avis des clients
 //TODO: petite vidéo qu'ils peuvent récupérer pour mettre en story ?
+
+export default function AdminContentUpload() {
+  return(
+    <div>HEllo</div>
+  )
+}
