2f8bc9217420cc0a021e357019aef97bff10256c
Yvens16 <yvensbelaston@gmail.com>
2021-07-13 10:43:16 +0200

    put the car choice picture on cloudinary for better performance

--- components/prices/CarChoice.js
+++ components/prices/CarChoice.js
@@ -1,8 +1,45 @@
 import React from 'react'
 import Image from 'next/image'
 import styles from './CarChoice.module.scss'
+import { buildUrl } from 'cloudinary-build-url';
 
 export default function CarChoice({ chooseCar }) {
+  const citadine = buildUrl('Premium car Wash/citadine', {
+    cloud: {
+      cloudName: process.env.NEXT_PUBLIC_SMARTLOOK_CLOUDINARY_NAME,
+    },
+    resize: {
+      width: 150,
+      height: 100,
+    }
+  });
+  const berline = buildUrl('Premium car Wash/berline_compact', {
+    cloud: {
+      cloudName: process.env.NEXT_PUBLIC_SMARTLOOK_CLOUDINARY_NAME,
+    },
+    resize: {
+      width: 200,
+      height: 150,
+    }
+  });
+  const routiere = buildUrl('Premium car Wash/petite_routiere', {
+    cloud: {
+      cloudName: process.env.NEXT_PUBLIC_SMARTLOOK_CLOUDINARY_NAME,
+    },
+    resize: {
+      width: 200,
+      height: 150,
+    }
+  });
+  const suv = buildUrl('Premium car Wash/grosse_routiere', {
+    cloud: {
+      cloudName: process.env.NEXT_PUBLIC_SMARTLOOK_CLOUDINARY_NAME,
+    },
+    resize: {
+      width: 200,
+      height: 150,
+    }
+  });
   return (
     <section className={styles.choice}>
       <div className={styles.title}>
@@ -13,19 +50,19 @@ export default function CarChoice({ chooseCar }) {
       </div>
       <div className={styles.car_types}>
         <div className={styles.car} onClick={() => chooseCar('citadine')}>
-          <Image alt='Citadine' src='/images/citadine.png' width='150' height='100'/>
+          <Image alt='Citadine' src={citadine} width='150' height='100'/>
           <p className={styles.car_name}>Petite citadine</p>
         </div>
         <div className={styles.car} onClick={() => chooseCar('berline')}>
-          <Image alt='Berline et compact' src='/images/berline_compact.png' width='200' height='150'/>
+          <Image alt='Berline et compact' src={berline} width='200' height='150'/>
           <p className={styles.car_name}>Berline & Compact</p>
         </div>
         <div className={styles.car} onClick={() => chooseCar('routiere')}>
-          <Image alt='Routière' src='/images/petite_routiere.png' width='200' height='150'/>
+          <Image alt='Routière' src={routiere} width='200' height='150'/>
           <p className={styles.car_name}>Routières & petit SUV</p>
         </div>
         <div className={styles.car} onClick={() => chooseCar('suv')}>
-          <Image alt='SUV' src='/images/grosse_routiere.png' width='200' height='150'/>
+          <Image alt='SUV' src={suv} width='200' height='150'/>
           <p className={styles.car_name}>Grand SUV</p>
         </div>
       </div>
--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ module.exports = {
     return config
   },
   images: {
+    domains: [
+      'res.cloudinary.com'
+    ],
     deviceSizes: [273, 322, 367, 500, 640, 750, 828, 900],
   },
 }
